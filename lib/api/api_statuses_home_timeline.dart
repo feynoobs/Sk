1a885a96b810d0c16b82bb026097be638996dc0e
feynoobs <p.w.feeniej@gmail.com>
2022-05-06 22:12:46 +0900

    hip!

---
+++ lib/api/api_statuses_home_timeline.dart
@@ -0,0 +1,16 @@
+import 'api_common.dart';
+
+class ApiStatusesHomeTimeline extends ApiCommon
+{
+    ApiStatusesHomeTimeline() : super('https://api.twitter.com/1.1/statuses/home_timeline.json', 'GET');
+
+    @override
+    Future<String> start(Map<String, String> params)
+    {
+        Map<String, String> fixedToken = {'oauth_token': params['oauth_token']!, 'oauth_token_secret': params['oauth_token_secret']!};
+        params.remove('oauth_token');
+        params.remove('oauth_token_secret');
+
+        return startMain(params, fixedToken);
+    }
+}
--- lib/screen/home_timeline.dart
+++ lib/screen/home_timeline.dart
@@ -9,6 +9,7 @@ import '../api/api_common.dart';
 import '../api/api_request_token.dart';
 import '../api/api_access_token.dart';
 import '../api/api_users_show.dart';
+import '../api/api_statuses_home_timeline.dart';
 import '../utility/utility.dart';
 
 class HomeTimeline extends StatefulWidget
@@ -56,39 +57,54 @@ class _HomeTimelineState extends State<HomeTimeline>
                                 arguments: params
                             )
                             .then((dynamic callback) {
-                                String query2 = (callback as String).replaceAll('${ApiCommon.CALLBACK_URL}?', '');
-                                Map<String, String> params2 = Utility.splitQuery(query2);
-                                // 認証拒否された場合は処理しない
-                                // 拒否されたばあい「denied」が付与されるので否定
-                                if (params2.containsKey('denied') == false) {
-                                    params2['oauth_token_secret'] = params['oauth_token_secret']!;
-                                    _logger.e(params2);
-                                    ApiAccessToken().start(params2)
-                                        .then((String query3) {
-                                            Map<String, String> params3 = Utility.splitQuery(query3);
-                                            authData = params3;
-                                            Map<String, String> userData = {'oauth_token': params3['oauth_token']!, 'oauth_token_secret': params3['oauth_token_secret']!, 'user_id': params3['user_id']!};
+                                // nullが帰ってくることがある
+                                if (callback != null) {
+                                    String query2 = (callback as String).replaceAll('${ApiCommon.CALLBACK_URL}?', '');
+                                    Map<String, String> params2 = Utility.splitQuery(query2);
+                                    // 認証拒否された場合は処理しない
+                                    // 拒否されたばあい「denied」が付与されるので否定
+                                    if (params2.containsKey('denied') == false) {
+                                        params2['oauth_token_secret'] = params['oauth_token_secret']!;
+                                        _logger.e(params2);
+                                        ApiAccessToken().start(params2)
+                                            .then((String query3) {
+                                                Map<String, String> params3 = Utility.splitQuery(query3);
+                                                authData = params3;
+                                                Map<String, String> userData = {'oauth_token': params3['oauth_token']!, 'oauth_token_secret': params3['oauth_token_secret']!, 'user_id': params3['user_id']!};
 
-                                            return ApiUsersShow().start(userData);
-                                        })
-                                        .then((String json) {
-                                            _logger.e(json);
-                                            return database.rawInsert(
-                                                'INSERT INTO t_users(user_id, oauth_token, oauth_token_secret, my, data, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)',
-                                                [authData['user_id'], authData['oauth_token'], authData['oauth_token_secret'], (my + 1).toString(), json, Utility.now(), Utility.now()]
-                                            );
-                                        })
-                                        .then((int status) {
-                                            if (status != 0) {
-                                                prefs.setInt('my', my + 1)
-                                                    .then((bool retult) {
-                                                        _logger.e('OKKKKK!!!!');
-                                                    });
-                                            }
-                                        });
+                                                return ApiUsersShow().start(userData);
+                                            })
+                                            .then((String json) {
+                                                _logger.e(json);
+                                                return database.rawInsert(
+                                                    'INSERT INTO t_users(user_id, oauth_token, oauth_token_secret, my, data, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)',
+                                                    [authData['user_id'], authData['oauth_token'], authData['oauth_token_secret'], (my + 1).toString(), json, Utility.now(), Utility.now()]
+                                                );
+                                            })
+                                            .then((int status) {
+                                                if (status != 0) {
+                                                    prefs.setInt('my', my + 1)
+                                                        .then((bool retult) {
+                                                            _logger.e('OKKKKK!!!!');
+                                                        });
+                                                }
+                                            });
+                                    }
                                 }
                             });
                         });
+                }
+                else {
+                    Map<String, String> requestData = {
+                        'oauth_token': user[0]['oauth_token'] as String,
+                        'oauth_token_secret': user[0]['oauth_token_secret'] as String,
+                        'count': 200.toString(),
+                        'exclude_replies': false.toString(),
+                        'contributor_details': false.toString(),
+                        'include_rts': true.toString(),
+                        'tweet_mode': 'extended'
+                    };
+                    ApiStatusesHomeTimeline().start(requestData);
                 }
             });
     }
