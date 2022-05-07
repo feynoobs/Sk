1cf857319be2d79e9a80610609d265ea1c68b57b
feynoobs <p.w.feeniej@gmail.com>
2022-05-08 01:28:45 +0900

    寝る

--- lib/screen/home_timeline.dart
+++ lib/screen/home_timeline.dart
@@ -27,6 +27,94 @@ class _HomeTimelineState extends State<HomeTimeline>
 {
     final Logger _logger = Logger();
 
+    Future<void> _getHomeTimeline()
+    {
+        Completer<void> computer = Completer<void>();
+        late SharedPreferences prefs;
+        late Database database;
+
+        SharedPreferences.getInstance()
+        .then((SharedPreferences p) {
+            prefs = p;
+            return DB.getInstance();
+        })
+        .then((Database db) {
+            int my = prefs.getInt('my') ?? 0;
+            database = db;
+            return db.rawQuery('SELECT oauth_token, oauth_token_secret FROM t_users WHERE my = ?', [my.toString()]);
+        })
+        .then((List<Map<String, Object?>> user) {
+            if (user.isNotEmpty == true) {
+                Map<String, String> requestData = {
+                    'oauth_token': user[0]['oauth_token'] as String,
+                    'oauth_token_secret': user[0]['oauth_token_secret'] as String,
+                    'count': 200.toString(),
+                    'exclude_replies': false.toString(),
+                    'contributor_details': false.toString(),
+                    'include_rts': true.toString(),
+                    'tweet_mode': 'extended'
+                };
+                ApiStatusesHomeTimeline().start(requestData)
+                .then((String jsonString) {
+                    List<dynamic> jsonObject = json.decode(jsonString);
+                    database.transaction((Transaction txn) {
+                        Completer<void> txnComputer = Completer<void>();
+                        int complate = 0;
+                        jsonObject.forEach((tweet) {
+                            txn.rawInsert(
+                                'INSERT INTO t_time_lines(tweet_id, reply_tweet_id, user_id, data, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)',
+                                [tweet['id'], tweet['in_reply_to_user_id'], tweet['user']['id'], tweet, Utility.now(), Utility.now()]
+                            )
+                            .then((int status) {
+                                if (status != 0) {
+                                    ++complate;
+                                    if (complate >= jsonObject.length) {
+                                        return txnComputer.complete();
+                                    }
+                                }
+                            });
+                        });
+                        return txnComputer.future;
+                    })
+                    .then((_) {
+                        return computer.complete();
+                    });
+                });
+            }
+        });
+
+
+        return computer.future;
+    }
+
+    Widget _memuItem(int id, int myUserId, Map<String, dynamic> jsonObject)
+    {
+        return Card(
+            child: Column(
+                children: <Widget>[
+                    Row(
+                        children: <Widget>[
+                            Column(
+                                children: <Widget>[
+                                    RichText(
+                                        overflow: TextOverflow.ellipsis,
+                                        text: TextSpan(
+                                            children: <InlineSpan>[
+                                                TextSpan(text: jsonObject['name']),
+                                                TextSpan(text: '@' + jsonObject['screen_name'], style: const TextStyle(fontStyle: FontStyle.italic))
+                                            ],
+                                        )
+                                    ),
+                                    Text(jsonObject['created_at'])
+                                ]
+                            )
+                        ]
+                    )
+                ],
+            )
+        );
+    }
+
     @override
     void initState()
     {
@@ -36,101 +124,76 @@ class _HomeTimelineState extends State<HomeTimeline>
         late SharedPreferences prefs;
 
         SharedPreferences.getInstance()
-            .then((SharedPreferences p) {
-                prefs = p;
-                return DB.getInstance();
-            })
-            .then((Database db) {
+        .then((SharedPreferences p) {
+            prefs = p;
+            return DB.getInstance();
+        })
+        .then((Database db) {
+            int my = prefs.getInt('my') ?? 0;
+            database = db;
+            return db.rawQuery('SELECT oauth_token, oauth_token_secret FROM t_users WHERE my = ?', [my.toString()]);
+        })
+        .then((List<Map<String, Object?>> user) {
+            if (user.isEmpty == true) {
+                late Map<String, String> authData;
                 int my = prefs.getInt('my') ?? 0;
-                database = db;
-                return db.rawQuery('SELECT oauth_token, oauth_token_secret FROM t_users WHERE my = ?', [my.toString()]);
-            })
-            .then((List<Map<String, Object?>> user) {
-                if (user.isEmpty == true) {
-                    late Map<String, String> authData;
-                    int my = prefs.getInt('my') ?? 0;
 
-                    ApiRequestToken().start({})
-                        .then((String query) {
-                            Map<String, String> params = Utility.splitQuery(query);
+                ApiRequestToken().start({})
+                .then((String query) {
+                    Map<String, String> params = Utility.splitQuery(query);
 
-                            return Navigator.pushNamed(
-                                context,
-                                'authentication',
-                                arguments: params
-                            )
-                            .then((dynamic callback) {
-                                // nullが帰ってくることがある
-                                if (callback != null) {
-                                    String query2 = (callback as String).replaceAll('${ApiCommon.CALLBACK_URL}?', '');
-                                    Map<String, String> params2 = Utility.splitQuery(query2);
-                                    // 認証拒否された場合は処理しない
-                                    // 拒否されたばあい「denied」が付与されるので否定
-                                    if (params2.containsKey('denied') == false) {
-                                        params2['oauth_token_secret'] = params['oauth_token_secret']!;
-                                        _logger.e(params2);
-                                        ApiAccessToken().start(params2)
-                                            .then((String query3) {
-                                                Map<String, String> params3 = Utility.splitQuery(query3);
-                                                authData = params3;
-                                                Map<String, String> userData = {'oauth_token': params3['oauth_token']!, 'oauth_token_secret': params3['oauth_token_secret']!, 'user_id': params3['user_id']!};
+                    return Navigator.pushNamed(
+                        context,
+                        'authentication',
+                        arguments: params
+                    )
+                    .then((dynamic callback) {
+                        // nullが帰ってくることがある
+                        if (callback != null) {
+                            String query2 = (callback as String).replaceAll('${ApiCommon.CALLBACK_URL}?', '');
+                            Map<String, String> params2 = Utility.splitQuery(query2);
+                            // 認証拒否された場合は処理しない
+                            // 拒否されたばあい「denied」が付与されるので否定
+                            if (params2.containsKey('denied') == false) {
+                                params2['oauth_token_secret'] = params['oauth_token_secret']!;
+                                _logger.e(params2);
+                                ApiAccessToken().start(params2)
+                                .then((String query3) {
+                                    Map<String, String> params3 = Utility.splitQuery(query3);
+                                    authData = params3;
+                                    Map<String, String> userData = {'oauth_token': params3['oauth_token']!, 'oauth_token_secret': params3['oauth_token_secret']!, 'user_id': params3['user_id']!};
 
-                                                return ApiUsersShow().start(userData);
-                                            })
-                                            .then((String json) {
-                                                return database.rawInsert(
-                                                    'INSERT INTO t_users(user_id, oauth_token, oauth_token_secret, my, data, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)',
-                                                    [authData['user_id'], authData['oauth_token'], authData['oauth_token_secret'], (my + 1).toString(), json, Utility.now(), Utility.now()]
-                                                );
-                                            })
-                                            .then((int status) {
-                                                if (status != 0) {
-                                                    prefs.setInt('my', my + 1)
-                                                        .then((bool retult) {
-                                                            _logger.e('OKKKKK!!!!');
-                                                        });
-                                                }
-                                            });
-                                    }
-                                }
-                            });
-                        });
-                }
-                else {
-                    Map<String, String> requestData = {
-                        'oauth_token': user[0]['oauth_token'] as String,
-                        'oauth_token_secret': user[0]['oauth_token_secret'] as String,
-                        'count': 200.toString(),
-                        'exclude_replies': false.toString(),
-                        'contributor_details': false.toString(),
-                        'include_rts': true.toString(),
-                        'tweet_mode': 'extended'
-                    };
-                    ApiStatusesHomeTimeline().start(requestData)
-                        .then((String jsonString) {
-                            List<dynamic> jsonObject = json.decode(jsonString);
-                            return database.transaction((Transaction txn) {
-                                Completer<void> computer = Completer<void>();
-                                int complate = 0;
-                                jsonObject.forEach((tweet) {
-                                    txn.rawInsert(
-                                        'INSERT INTO t_time_lines(tweet_id, reply_tweet_id, user_id, data, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)',
-                                        [tweet['id'], tweet['in_reply_to_user_id'], tweet['user']['id'], tweet, Utility.now(), Utility.now()]
-                                    )
-                                        .then((int status) {
-                                            ++complate;
-                                            if (complate >= jsonObject.length) {
-                                                return computer.complete();
+                                    return ApiUsersShow().start(userData);
+                                })
+                                .then((String json) {
+                                    return database.rawInsert(
+                                        'INSERT INTO t_users(user_id, oauth_token, oauth_token_secret, my, data, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)',
+                                        [authData['user_id'], authData['oauth_token'], authData['oauth_token_secret'], (my + 1).toString(), json, Utility.now(), Utility.now()]
+                                    );
+                                })
+                                .then((int status) {
+                                    if (status != 0) {
+                                        prefs.setInt('my', my + 1)
+                                        .then((bool retult) {
+                                            if (retult == true) {
+                                                _getHomeTimeline()
+                                                .then((_) {
+                                                    setState(() {
+
+                                                    });
+                                                });
                                             }
                                         });
+                                    }
                                 });
-                                return computer.future;
-                            });
-                        })
-                        .then((_) {
-                        });
-                }
-            });
+                            }
+                        }
+                    });
+                });
+            }
+            else {
+            }
+        });
     }
 
     @override
